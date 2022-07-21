cc17b1b03a0905aa9a68b1e707349d04bc432218
doujiang24 <doujiang24@gmail.com>
2022-07-21 09:45:32 +0800

    fix invalid string data pointer.

--- internal/gocore/read.go
+++ internal/gocore/read.go
@@ -311,9 +311,13 @@ func ReadString(a core.Address, t *Type, r reader, p *Process) {
 	len := r.ReadInt(a.Add(ptrSize))
 
 	fmt.Printf("string, len: %v, data: 0x%x\n", len, data)
-	fmt.Printf("string value: \"")
-	for i := int64(0); i < len; i++ {
-		fmt.Printf("%c", r.ReadUint8(data.Add(i)))
+	if data != 0 {
+		fmt.Printf("string value: \"")
+		for i := int64(0); i < len; i++ {
+			fmt.Printf("%c", r.ReadUint8(data.Add(i)))
+		}
+		fmt.Printf("\"\n")
+	} else {
+		fmt.Printf("Error: invalid string data pointer: %v", data)
 	}
-	fmt.Printf("\"\n")
 }
